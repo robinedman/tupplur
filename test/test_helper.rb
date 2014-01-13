b782e2618dd69711a4d716e33c671a00fb4b3eb5
robin <robin.edman@gmail.com>
2014-01-13 19:09:45 +0100

    Tweaked Turn output when running tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require "active_support/all"
 require "rack/test"
 require_relative "test_app/app"
 
+Turn.config.natural = true
+
 class TupplurTestCase < ActiveSupport::TestCase
   include Rack::Test::Methods
 
